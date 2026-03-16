bbfa34995fd4b5b6710f37b621d77aaeb0221283
RetroJet <rretrojet@gmail.com>
2026-03-16 23:26:10 +0100

    Initial Commit

---
+++ NatifeTask1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NatifeTask1
+//
+//  Created by Nazar on 16.03.2026.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
